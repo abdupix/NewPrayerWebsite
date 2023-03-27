d04aece54237132819536e7c3a01ba9db78847f1
Abdu <47911637+abdupix@users.noreply.github.com>
2023-03-28 00:05:34 +1300

    Add files via upload

--- calendar.js
+++ calendar.js
@@ -86,3 +86,42 @@ function getPrayerTimes() {
       console.error(error);
     });
 }
+
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('modalOverlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+modalOverlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.modal-active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  })
+})
+
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal){
+  if(modal == null) return
+  modal.classList.add('modal-active')
+  overlay.classList.add('modal-active')
+}
+
+function closeModal(modal){
+  if(modal == null) return
+  modal.classList.remove('modal-active')
+  overlay.classList.remove('modal-active')
+}
