ba8eb1ba49e8d26d6fd2008475c187569783321d
Muhammed Faizel Adam <mfadam786@gmail.com>
2023-04-05 16:54:45 +1200

    Reorder and renamed containers and their id's
    Halaal directory would be scrolled to before the calendar but the
    calendar was the third container. Fixed this and renamed them
    accordingly.

--- calendar.js
+++ calendar.js
@@ -170,9 +170,9 @@ function closeModal(modal) {
 
 const selectors = [
   "#mainContainer",
-  "#secondMainContainer",
-  "#thirdMainContainer",
-  "#fourthMainContainer",
+  "#hadithMainContainer",
+  "#calendarMainContainer",
+  "#halaalDirectoryMainContainer",
 ];
 
 const options = {
